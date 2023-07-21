06377f00e146b32897de271fcaa1e71f2795e9d7
Edwinfpirajan <efpa1998@hotmail.com>
2023-07-21 17:20:13 -0500

    add test

--- internal/config/db_kactus.go
+++ internal/config/db_kactus.go
@@ -1,45 +1,72 @@
+// package config
+
+// import (
+// 	"log"
+// 	"os"
+
+// 	"github.com/joho/godotenv"
+// 	"gorm.io/driver/sqlserver"
+// 	"gorm.io/gorm"
+// )
+
+// var KDB *gorm.DB
+
+// func KactusDB() {
+// 	loadEnvKactus()
+
+// 	dsn := KactusDSN()
+
+// 	db, err := gorm.Open(sqlserver.Open(dsn), &gorm.Config{})
+// 	if err != nil {
+// 		log.Fatalf("Error connecting to database: %v", err)
+// 	}
+
+// 	KDB = db
+
+// 	log.Println("Database connection successfully established")
+// }
+
+// func loadEnvKactus() {
+// 	err := godotenv.Load()
+// 	if err != nil {
+// 		log.Fatalf("Error loading .env file")
+// 	}
+// }
+
+// func KactusDSN() string {
+// 	domain := os.Getenv("KDB_DOMAIN")
+// 	user := os.Getenv("KDB_USER")
+// 	pass := os.Getenv("KDB_PASS")
+// 	port := os.Getenv("KDB_PORT")
+// 	database := os.Getenv("KDB_DATABASE")
+
+// 	dsn := "sqlserver://" + user + ":" + pass + "@" + domain + ":" + port + "?database=" + database
+// 	return dsn
+// }
+
 package config
 
 import (
 	"log"
-	"os"
 
-	"github.com/joho/godotenv"
 	"gorm.io/driver/sqlserver"
 	"gorm.io/gorm"
 )
 
 var KDB *gorm.DB
 
-func KactusDB() {
-	loadEnvKactus()
+func init() {
+	domain := "10.100.0.18"
+	user := "ASISTENCIA"
+	pass := "*T3cn0l0g14-*"
+	port := "1433"
+	database := "KACTUS"
 
-	dsn := KactusDSN()
-
+	dsn := "sqlserver://" + user + ":" + pass + "@" + domain + ":" + port + "?database=" + database
 	db, err := gorm.Open(sqlserver.Open(dsn), &gorm.Config{})
 	if err != nil {
 		log.Fatalf("Error connecting to database: %v", err)
 	}
-
 	KDB = db
-
-	log.Println("Database connection successfully established")
-}
-
-func loadEnvKactus() {
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatalf("Error loading .env file")
-	}
-}
-
-func KactusDSN() string {
-	domain := os.Getenv("KDB_DOMAIN")
-	user := os.Getenv("KDB_USER")
-	pass := os.Getenv("KDB_PASS")
-	port := os.Getenv("KDB_PORT")
-	database := os.Getenv("KDB_DATABASE")
-
-	dsn := "sqlserver://" + user + ":" + pass + "@" + domain + ":" + port + "?database=" + database
-	return dsn
+	log.Println("Conexión a Kactus con éxito")
 }
