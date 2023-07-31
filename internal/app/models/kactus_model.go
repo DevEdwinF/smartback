b18baa7d322e809218fca78a664fa762aa8e0977
Edwinfpirajan <efpa1998@hotmail.com>
2023-07-31 16:40:43 -0500

    change type struct

--- internal/app/models/kactus_model.go
+++ internal/app/models/kactus_model.go
@@ -1,19 +1,11 @@
 package models
 
-import "time"
-
 // type BiEmple struct {
 // 	CodEmpl string
 // }
 
-type collaborators struct {
-	NúmeroIdentificación string    `gorm:"column:cod_inte"`
-	Nombres              string    `gorm:"column:nom_empl"`
-	Apellidos            string    `gorm:"column:ape_empl"`
-	Cargo                string    `gorm:"column:nom_carg"`
-	FechaIngreso         time.Time `gorm:"column:fec_ingr"`
-	CorreoCorporativo    string    `gorm:"column:box_mail"`
-	CorreoPersonal       string    `gorm:"column:eee_mail"`
-	NombresJefeInme      string    `gorm:"column:nom_empl"`
-	ApellidosJefeInme    string    `gorm:"column:ape_empl"`
+type NmContr struct {
+	CodEmpl string
+	NomEmpl string
+	ApeEmpl string
 }
--- internal/app/services/kactus_service.go
+++ internal/app/services/kactus_service.go
@@ -7,29 +7,16 @@ import (
 
 func GetAllColab() ([]models.Collaborators, error) {
 	collaborators := []models.Collaborators{}
-	err := config.KDB.Table("nm_contr").Select("*").Scan(&collaborators).Error
+	err := config.KDB.Table("NM_CONTR").
+		Select("bi_emple.cod_inte AS NúmeroIdentificación, bi_emple.nom_empl AS Nombres, bi_emple.ape_empl AS Apellidos, BI_CARGO.nom_carg AS Cargo, NM_CONTR.fec_ingr AS FechaIngreso, bi_emple.box_mail AS CorreoCorporativo, bi_emple.eee_mail AS CorreoPersonal, NomJefe.nom_empl AS NombresJefeInme, NomJefe.ape_empl AS ApellidosJefeInme").
+		Joins("INNER JOIN bi_emple ON NM_CONTR.cod_empl = bi_emple.cod_empl").
+		Joins("INNER JOIN BI_CARGO ON NM_CONTR.cod_carg = BI_CARGO.cod_carg").
+		Joins("INNER JOIN bi_emple AS NomJefe ON NM_CONTR.cod_frep = NomJefe.cod_empl").
+		Scan(&collaborators).Error
+
 	if err != nil {
 		return nil, err
 	}
 
-	return collaborators, err
+	return collaborators, nil
 }
-
-// SELECT
-// bi_emple.cod_inte AS NúmeroIdentificación,
-// bi_emple.nom_empl AS Nombres,
-// bi_emple.ape_empl AS Apellidos,
-// nom_carg AS Cargo,
-// fec_ingr AS FechaIngreso,
-// bi_emple.box_mail AS CorreoCorporativo,
-// bi_emple.eee_mail AS CorreoPersonal,
-// NomJefe.nom_empl AS NombresJefeInme,
-// NomJefe.ape_empl AS ApellidosJefeInme
-// FROM
-// NM_CONTR
-// INNER JOIN
-// bi_emple ON NM_CONTR.cod_empl = bi_emple.cod_empl
-// INNER JOIN
-// BI_CARGO ON NM_CONTR.cod_carg = BI_CARGO.cod_carg
-// INNER JOIN
-// bi_emple AS NomJefe ON NM_CONTR.cod_frep = NomJefe.cod_empl
