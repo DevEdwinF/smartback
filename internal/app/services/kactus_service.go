4d4a3b97d47bd944d39fdb4a8edc58a3b8b82d89
Edwinfpirajan <efpa1998@hotmail.com>
2023-07-31 16:27:32 -0500

    update sql

--- internal/app/services/kactus_service.go
+++ internal/app/services/kactus_service.go
@@ -7,25 +7,25 @@ import (
 
 func GetAllColab() ([]models.Collaborators, error) {
 	collaborators := []models.Collaborators{}
-	err := config.KDB.Raw(`
-	        SELECT
-	            bi_emple.cod_inte AS NúmeroIdentificación,
-	            bi_emple.nom_empl AS Nombres,
-	            bi_emple.ape_empl AS Apellidos,
-	            nom_carg AS Cargo,
-	            fec_ingr AS FechaIngreso,
-	            bi_emple.box_mail AS CorreoCorporativo,
-	            bi_emple.eee_mail AS CorreoPersonal,
-	            NomJefe.nom_empl AS NombresJefeInme,
-	            NomJefe.ape_empl AS ApellidosJefeInme
-	        FROM
-	            NM_CONTR
-	        INNER JOIN
-	            bi_emple ON NM_CONTR.cod_empl = bi_emple.cod_empl
-	        INNER JOIN
-	            BI_CARGO ON NM_CONTR.cod_carg = BI_CARGO.cod_carg
-	        INNER JOIN
-	            bi_emple AS NomJefe ON NM_CONTR.cod_frep = NomJefe.cod_empl
-	    `).Scan(&collaborators).Error
+	err := config.KDB.Table("nm_contr").Select("*").Scan(&collaborators).Error
 	return collaborators, err
 }
+
+// SELECT
+// bi_emple.cod_inte AS NúmeroIdentificación,
+// bi_emple.nom_empl AS Nombres,
+// bi_emple.ape_empl AS Apellidos,
+// nom_carg AS Cargo,
+// fec_ingr AS FechaIngreso,
+// bi_emple.box_mail AS CorreoCorporativo,
+// bi_emple.eee_mail AS CorreoPersonal,
+// NomJefe.nom_empl AS NombresJefeInme,
+// NomJefe.ape_empl AS ApellidosJefeInme
+// FROM
+// NM_CONTR
+// INNER JOIN
+// bi_emple ON NM_CONTR.cod_empl = bi_emple.cod_empl
+// INNER JOIN
+// BI_CARGO ON NM_CONTR.cod_carg = BI_CARGO.cod_carg
+// INNER JOIN
+// bi_emple AS NomJefe ON NM_CONTR.cod_frep = NomJefe.cod_empl
