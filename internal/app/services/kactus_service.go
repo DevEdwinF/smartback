f53488f2b36db2069292dd4b9344b02939555727
Edwinfpirajan <efpa1998@hotmail.com>
2023-07-31 16:19:12 -0500

    test sql

--- internal/app/services/kactus_service.go
+++ internal/app/services/kactus_service.go
@@ -9,23 +9,32 @@ func GetAllColab() ([]models.NmContr, error) {
 	collaborators := []models.NmContr{}
 	err := config.DB.Raw(`
         SELECT
-            bi_emple.cod_inte AS NúmeroIdentificación,
-            bi_emple.nom_empl AS Nombres,
-            bi_emple.ape_empl AS Apellidos,
-            nom_carg AS Cargo,
-            fec_ingr AS FechaIngreso,
-            bi_emple.box_mail AS CorreoCorporativo,
-            bi_emple.eee_mail AS CorreoPersonal,
-            NomJefe.nom_empl AS NombresJefeInme,
-            NomJefe.ape_empl AS ApellidosJefeInme
+           *
         FROM
-            NM_CONTR
-        INNER JOIN
-            bi_emple ON NM_CONTR.cod_empl = bi_emple.cod_empl
-        INNER JOIN
-            BI_CARGO ON NM_CONTR.cod_carg = BI_CARGO.cod_carg
-        INNER JOIN
-            bi_emple AS NomJefe ON NM_CONTR.cod_frep = NomJefe.cod_empl
+            bi_emple
     `).Scan(&collaborators).Error
 	return collaborators, err
+	// func GetAllColab() ([]models.NmContr, error) {
+	// 	collaborators := []models.NmContr{}
+	// 	err := config.DB.Raw(`
+	//         SELECT
+	//             bi_emple.cod_inte AS NúmeroIdentificación,
+	//             bi_emple.nom_empl AS Nombres,
+	//             bi_emple.ape_empl AS Apellidos,
+	//             nom_carg AS Cargo,
+	//             fec_ingr AS FechaIngreso,
+	//             bi_emple.box_mail AS CorreoCorporativo,
+	//             bi_emple.eee_mail AS CorreoPersonal,
+	//             NomJefe.nom_empl AS NombresJefeInme,
+	//             NomJefe.ape_empl AS ApellidosJefeInme
+	//         FROM
+	//             NM_CONTR
+	//         INNER JOIN
+	//             bi_emple ON NM_CONTR.cod_empl = bi_emple.cod_empl
+	//         INNER JOIN
+	//             BI_CARGO ON NM_CONTR.cod_carg = BI_CARGO.cod_carg
+	//         INNER JOIN
+	//             bi_emple AS NomJefe ON NM_CONTR.cod_frep = NomJefe.cod_empl
+	//     `).Scan(&collaborators).Error
+	// 	return collaborators, err
 }
