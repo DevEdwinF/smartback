d8ba9a490ec95cb67e6ec39055e273c00096b8bd
Edwinfpirajan <efpa1998@hotmail.com>
2023-07-31 16:44:30 -0500

    gorm used

--- internal/app/models/kactus_model.go
+++ internal/app/models/kactus_model.go
@@ -5,7 +5,7 @@ package models
 // }
 
 type NmContr struct {
-	CodEmpl string
-	NomEmpl string
-	ApeEmpl string
+	CodEmpl string `gorm:"column:cod_empl"`
+	NomEmpl string `gorm:"column:nom_empl"`
+	ApeEmpl string `gorm:"column:ape_empl"`
 }
