f417e5be3483d9772d643ef5260ef5f485d41302
Edwinfpirajan <efpa1998@hotmail.com>
2023-08-01 16:20:56 -0500

    document fixed GetAllColabFunction

--- internal/app/services/kactus_service.go
+++ internal/app/services/kactus_service.go
@@ -8,7 +8,7 @@ import (
 func GetAllColab() ([]models.NmContr, error) {
 	collaborators := []models.NmContr{}
 	err := config.KDB.Table("NM_CONTR").
-		Select("bi_emple.cod_inte AS n_identification, bi_emple.nom_empl AS f_name, bi_emple.ape_empl AS l_name, BI_CARGO.nom_carg AS position, NM_CONTR.fec_ingr AS date, bi_emple.box_mail AS b_mail, bi_emple.eee_mail AS e_mail, NomJefe.nom_empl AS fn_leader, NomJefe.ape_empl AS ln_leader").
+		Select("bi_emple.cod_inte AS document, bi_emple.nom_empl AS f_name, bi_emple.ape_empl AS l_name, BI_CARGO.nom_carg AS position, NM_CONTR.fec_ingr AS date, bi_emple.box_mail AS b_mail, bi_emple.eee_mail AS e_mail, NomJefe.nom_empl AS fn_leader, NomJefe.ape_empl AS ln_leader").
 		Joins("INNER JOIN bi_emple ON NM_CONTR.cod_empl = bi_emple.cod_empl").
 		Joins("INNER JOIN BI_CARGO ON NM_CONTR.cod_carg = BI_CARGO.cod_carg").
 		Joins("INNER JOIN bi_emple AS NomJefe ON NM_CONTR.cod_frep = NomJefe.cod_empl").
