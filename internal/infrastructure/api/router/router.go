f7cd67e796534c25c4371a14d5f9f0c648426d06
Edwinfpirajan <efpa1998@hotmail.com>
2023-08-02 17:05:42 -0500

    update controllers

--- internal/infrastructure/api/router/router.go
+++ internal/infrastructure/api/router/router.go
@@ -15,7 +15,7 @@ func GlobalRouter(e *echo.Echo) {
 	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
 		AllowOrigins: []string{"*"},
 		AllowMethods: []string{"*"},
-		// AllowHeaders: []string{"*"},
+		AllowHeaders: []string{"*"},
 	}))
 	e.Use(middleware.LoggerWithConfig(middleware.LoggerConfig{
 		Format: "method=${method}, uri=${uri}, status=${status}\n",
