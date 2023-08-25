a053f5f8f970467268e191a9eaa8b0c7ba16fe9c
Edwinfpirajan <efpa1998@hotmail.com>
2023-08-25 08:13:25 -0500

    update main.go

--- cmd/main.go
+++ cmd/main.go
@@ -1,10 +1,8 @@
 package main
 
 import (
-	"github.com/DevEdwinF/smartback.git/internal/app/services"
 	"github.com/DevEdwinF/smartback.git/internal/config"
-	router "github.com/DevEdwinF/smartback.git/internal/infrastructure/api/router"
-
+	"github.com/DevEdwinF/smartback.git/internal/infrastructure/api/router"
 	"github.com/labstack/echo/v4"
 )
 
@@ -12,7 +10,7 @@ func main() {
 	config.ConnectDB()
 	config.KactusDB()
 	e := echo.New()
-	services.RunCronJob()
+	// services.RunCronJob()
 
 	router.GlobalRouter(e)
 
