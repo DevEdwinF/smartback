c13b3c69a2566f83065a075bb0cf2e50119d59f8
Edwinfpirajan <efpa1998@hotmail.com>
2023-08-30 08:07:00 -0500

    acitave other function

--- migrations/scheduleMigrations.go
+++ migrations/scheduleMigrations.go
@@ -19,56 +19,13 @@ func NewScheduleMigrationService() *ScheduleMigrationService {
 func (s *ScheduleMigrationService) MigrationSchedule(schedule models.Schedules) {
 }
 
-func UploadExcelAndAssignSchedules(c echo.Context) error {
-	file, err := c.FormFile("file")
-	if err != nil {
-		return echo.NewHTTPError(http.StatusBadRequest, "Error al cargar el archivo")
-	}
-
-	excelFile, err := excelize.OpenFile(file.Filename)
-	if err != nil {
-		return echo.NewHTTPError(http.StatusInternalServerError, "Error al abrir el archivo Excel")
-	}
-
-	rows, err := excelFile.GetRows("horario")
-	if err != nil {
-		return echo.NewHTTPError(http.StatusInternalServerError, "Error al leer las filas del archivo Excel")
-	}
-
-	for _, row := range rows {
-		document := row[0]
-		day := row[1]
-		arrivalTime := row[2]
-		departureTime := row[3]
-
-		query := fmt.Sprintf(`INSERT INTO "schedules" ("day","arrival_time","departure_time","fk_collaborator_id")
-                             SELECT ?, ?, ?, c."id"
-                             FROM "collaborators" c
-                             WHERE c."document" = ?
-                             RETURNING "id"`)
-		var scheduleID int
-		err := config.DB.Raw(query, day, arrivalTime, departureTime, document).Row().Scan(&scheduleID)
-		if err != nil {
-			continue
-		}
-	}
-
-	return c.JSON(http.StatusOK, "Carga masiva completada")
-}
-
 // func UploadExcelAndAssignSchedules(c echo.Context) error {
 // 	file, err := c.FormFile("file")
 // 	if err != nil {
 // 		return echo.NewHTTPError(http.StatusBadRequest, "Error al cargar el archivo")
 // 	}
 
-// 	src, err := file.Open()
-// 	if err != nil {
-// 		return echo.NewHTTPError(http.StatusInternalServerError, "Error al abrir el archivo subido")
-// 	}
-// 	defer src.Close()
-
-// 	excelFile, err := excelize.OpenReader(src)
+// 	excelFile, err := excelize.OpenFile(file.Filename)
 // 	if err != nil {
 // 		return echo.NewHTTPError(http.StatusInternalServerError, "Error al abrir el archivo Excel")
 // 	}
@@ -89,7 +46,6 @@ func UploadExcelAndAssignSchedules(c echo.Context) error {
 //                              FROM "collaborators" c
 //                              WHERE c."document" = ?
 //                              RETURNING "id"`)
-
 // 		var scheduleID int
 // 		err := config.DB.Raw(query, day, arrivalTime, departureTime, document).Row().Scan(&scheduleID)
 // 		if err != nil {
@@ -99,3 +55,47 @@ func UploadExcelAndAssignSchedules(c echo.Context) error {
 
 // 	return c.JSON(http.StatusOK, "Carga masiva completada")
 // }
+
+func UploadExcelAndAssignSchedules(c echo.Context) error {
+	file, err := c.FormFile("file")
+	if err != nil {
+		return echo.NewHTTPError(http.StatusBadRequest, "Error al cargar el archivo")
+	}
+
+	src, err := file.Open()
+	if err != nil {
+		return echo.NewHTTPError(http.StatusInternalServerError, "Error al abrir el archivo subido")
+	}
+	defer src.Close()
+
+	excelFile, err := excelize.OpenReader(src)
+	if err != nil {
+		return echo.NewHTTPError(http.StatusInternalServerError, "Error al abrir el archivo Excel")
+	}
+
+	rows, err := excelFile.GetRows("horario")
+	if err != nil {
+		return echo.NewHTTPError(http.StatusInternalServerError, "Error al leer las filas del archivo Excel")
+	}
+
+	for _, row := range rows {
+		document := row[0]
+		day := row[1]
+		arrivalTime := row[2]
+		departureTime := row[3]
+
+		query := fmt.Sprintf(`INSERT INTO "schedules" ("day","arrival_time","departure_time","fk_collaborator_id")
+                             SELECT ?, ?, ?, c."id"
+                             FROM "collaborators" c
+                             WHERE c."document" = ?
+                             RETURNING "id"`)
+
+		var scheduleID int
+		err := config.DB.Raw(query, day, arrivalTime, departureTime, document).Row().Scan(&scheduleID)
+		if err != nil {
+			continue
+		}
+	}
+
+	return c.JSON(http.StatusOK, "Carga masiva completada")
+}
