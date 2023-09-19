5a519a0af58e4c66366bf62c1223896fc6ca4592
Edwinfpirajan <efpa1998@hotmail.com>
2023-09-19 08:10:01 -0500

    test

--- internal/config/db_connect_postgres.go
+++ internal/config/db_connect_postgres.go
@@ -13,12 +13,12 @@ import (
 var DB *gorm.DB
 
 func ConnectDB() {
-	loadEnv()
+	// loadEnv()
 
-	dsn := buildDSN()
+	// dsn := buildDSN()
 	// dsn := "user=asistencia password=1234456 dbname=asistencia port=5432 sslmode=disable"
 	// PGPASSWORD=7xt3Vx6eAevhZTMmSiGJ psql -h containers-us-west-210.railway.app -U postgres -p 7112 -d railway
-	// dsn := "host=localhost user=asistencias password=123456 dbname=asistencias port=5432 sslmode=disable"
+	dsn := "host=localhost user=asistencias password=*T3cn0l0g14* dbname=asistenciasdb port=5432 sslmode=disable"
 
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
